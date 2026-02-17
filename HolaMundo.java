6a7d7d7eb0c3fd1206afdefd07719a115343eaf1
josecamilo21 <josepetrified@gmail.com>
2026-02-17 17:31:47 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+import java.util.*;
+/**
+ *
+ * @author B11
+ */
+public class HolaMundo {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String lluvia;
+        // Saludar Al Mundo
+        System.out.println("Hola Mundo. Bienvenidos a PROGCOM-B");
+        System.out.println("Esta lloviendo?");
+        //Estamos definiendo un objeto de tipo Scanner
+        Scanner play = new Scanner(System.in);
+        lluvia=play.nextLine();
+        
+        System.out.println("Tu respuesta fue: "+lluvia);
+        
+        if (lluvia=="si"){
+            System.out.println("Use sombrilla. guarde la ropa");
+        }else{
+            System.out.println("Salga tranquilo");
+        }
+    }
+    
+}
